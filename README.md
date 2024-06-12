77f8c02a90dc867651a68d6e6a7290baf5d2ade2
Ben Morrison <ben@Bens-MacBook-Air-3.local>
2024-06-12 13:38:40 -0400

    finished

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+DEPLOYED SITE: https://6669dac95d96850659f23227--grand-horse-4806be.netlify.app/
+
+
+
+
 # React + Vite
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,63 @@ import { puppyList } from "./data.js";
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const [isButtonClicked, setIsButtonClicked] = useState(false);
+
+  function addPuppy() {
+    const newPup = { ...puppyList[0] };
+    newPup.id = puppyList[puppyList.length - 1].id + 1;
+    newPup.name = "Sir Waddington's Brother";
+    newPup.age = 7;
+    const newList = [...puppies, newPup];
+    setPuppies(newList)
+    setIsButtonClicked(true)
+  }
+  function removePuppy() {
+    const newList = puppies.slice(0, -1);
+    setPuppies(newList);
+    if (featPupId === puppies[puppies.length - 1].id) {
+      setFeatPupId(null);
+    }
+    setIsButtonClicked(false);
+  }
+
+const featuredPup = puppies.find((pup)=> pup.id === featPupId)
   return (
     <>
       <div className="App">
+        <div id="side">
+          <div id="sep">
+              <h1 id="header">List of Pups</h1>
         {puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>;
+          return (
+            <p
+              onClick={() => {setFeatPupId(puppy.id);}}
+              key={puppy.id}
+            >
+              {puppy.name}
+            </p>
+          );
         })}
+          </div>
+        {featPupId && (
+        <div id="featPup">
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+            </div>
+          )} 
+          </div>
+        {!isButtonClicked && (
+          <button onClick={addPuppy} className="addButton">
+            Add Puppy
+          </button>
+        )}
+        {isButtonClicked && (
+          <button onClick={removePuppy} className="removeButton">Undo</button>
+        )}
       </div>
     </>
   );
