5e34c0665206cb2edc9d3ff0c0ee5f1c2a4cf6fc
Ben Morrison <ben@Bens-MacBook-Air-3.local>
2024-06-12 11:41:26 -0400

    implemented map

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { puppyList } from "./data.js";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [puppies, setPuppies] = useState(puppyList);
   return (
     <>
-   
+      <div className="App">
+        {puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>;
+        })}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
